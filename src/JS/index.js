8b9c39dcd47aa3449abd1bc705bd5d81df794e28
nicooniate <nicooniate02@gmail.com>
2025-04-04 15:59:40 -0300

    cors fixed

--- src/JS/index.js
+++ src/JS/index.js
@@ -4,12 +4,10 @@ import { PORT } from "./config.js";
 import path from 'path'
 import cors from "cors";
 
-
-
 const app = express(); // inicio express
 
 app.use(cors({
-    origin: "https://nicooniate.github.io" // Para que Render tenga CORS habilitado para aceptar peticiones desde GitHub Pages.
+    origin: "https://nicooniate.github.io"
 }));
 
 
@@ -19,5 +17,7 @@ app.use(paymentRoutes); // La app por defecto lee las rutas de pago
 
 app.use(express.static(path.resolve(''))) // path.resolve hace q al querer colocar una ruta de algun archivo busque desde el incio del proyecto y no dentro de la misma carpeta
 
-app.listen(PORT); // incio mi app en el port 3000
-console.log("Server on port", PORT);
+app.listen(PORT, ()=>{
+    console.log("Server on port", PORT);
+}); // incio mi app en el port 3000
+
